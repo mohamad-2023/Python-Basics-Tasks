14e982323b660a3013bb52dd179653aa8c4a6a2a
mohamad-2023 <mmm19870linkin@gmail.com>
2022-11-06 22:19:53 +0100

    variables tasks are complete

---
+++ 01-Variables/tasks-variables.py
@@ -0,0 +1,60 @@
+
+
+'''
+
+1. Create a boolean variable named x
+
+x = true
+
+2. Create an integer variable named y
+
+y = 5
+
+3. Create a float variable named z
+
+z = 3.0
+
+4. create a string variable named s
+
+s = "Ahmad"
+
+5. Convert the int variable to float
+
+a = 7
+float (a)
+
+6. Can we convert the str to int?
+yes we can example
+str = "12345"
+int(str)
+
+
+7. creat a list of numbers from 1 to 5
+
+list = [1 , 2 , 3 , 4, 5]
+
+
+8. Create a tuple from 10 to 15
+ tup = (10 , 11 , 12 , 13 , 14 , 15 )
+ 
+
+9. conver the list into tuple
+tuple(list)
+
+10. Create a dict of 3 values
+my_dict = {1: "Apple", 2:"Orange", 3:"quwi"
+
+11. Can we use semi colon ; with python?
+
+yes but it is not nesisary
+
+12. Python is interpreted or compiled ?
+
+python is interpreted language
+
+13. What is the diffrernces between low level & high level ?
+
+low level language handle withe machines and hard to understand from humans
+high level language handle have better interface witch makes it easer to understane from the programmers
+
+
