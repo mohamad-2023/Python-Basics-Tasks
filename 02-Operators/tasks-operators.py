e7717eee1e9868fc97d95673296b5925ac4990dd
mohamad-2023 <mmm19870linkin@gmail.com>
2022-11-07 19:17:47 +0100

    operators tasks

--- 02-Operators/tasks-operators.py
+++ 02-Operators/tasks-operators.py
@@ -1,2 +1,50 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+'''
+
+1. What is the differences between =, ==
+
+= mean equal
+== means a condition
+
+2. What do we mean by using !=
+
+!= means not equal
+
+3. What is the operator precedence
+
+that means * and / first then come + and -
+
+4. Create a variable x with a value of 10
+
+x = 10
+
+5. Increase x value by 15 using assignment operators
+
+x +=5
+
+6. Divide the x value by 5 using assignment operators
+
+x /=5
+
+7. Multiply the x value by 10 using assignment operators
+
+x *= 10
+
+8. Get module of x on 3 using assignment operator
+
+x %=3
+
+9. Using python print the module of 11 to 4
+
+x = 11 % 4
+
+10. Print the exponent of 2,3
+
+x = 2**3
+
+11. Divide 11 by 3 using python diivision
+
+x = 11 / 3
+
+12. Can we divide 11 by 3 and get an integer numbet
+
+yes : 11  // 3
