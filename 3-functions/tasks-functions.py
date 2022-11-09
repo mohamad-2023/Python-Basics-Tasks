1d3bdfcc8e15a0adcdb9cf9584b795e94fced478
mohamad-2023 <mmm19870linkin@gmail.com>
2022-11-09 18:30:51 +0100

    add function-tasks

---
+++ 3-functions/tasks-functions.py
@@ -0,0 +1,43 @@
+'''
+
+1. Create a simple function that takes 2 numbers and print their values
+
+def prt(x,y):
+    print (x)
+    print(y)
+
+2. Create a simple function that takes 2 numbers and retutn their values
+
+def rtn(x,y):
+    return (x)
+    return(y)
+
+3.In the above return function, usr keyword arguments when calling the function
+
+def rtn(x =7, y =8):
+    print (x,y)
+
+
+4.  In the above return function, give x and y default values of 0 and call the function with no arguments
+
+def rtn(x=0,y=0):
+    return (x)
+    return(y)
+5. Create a function that can take any number of arguments and print the sum of them
+
+A =(1,2,3,4,5,6,7,8,9)
+def sum-n= sum(*A)
+
+6. Create the same sum function using the lambda
+
+a = (sum(x+y))(8,4)
+
+7. Call the lambda function at the same definition line
+
+a = (sum(x+y))(8,4)
+
+8. Write the defference between the local variable and global variable
+
+local variable has the value just in a part of the script
+global variable has the value in the hall programm
+
