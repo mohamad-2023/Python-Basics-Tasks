2082f53ead5d4a0c73c08227a99e07aecef1f4e2
mohamad-2023 <mmm19870linkin@gmail.com>
2022-11-10 03:37:05 +0100

    tasks-conditions

---
+++ 4-conditions/tasks-conditions.py
@@ -0,0 +1,63 @@
+
+'''
+
+1. Check if 10 bigger than 15 or not
+
+if 10 >= 5 :
+     print (true)
+ els :print (false)
+      
+ 2. if 10 not bigger than 15 print x i smaller than 15
+
+
+
+3.In which case use all
+
+
+4. What is the differences between all, and
+
+they are same but all is better for maney conditions
+
+5. What is the differences between any, or
+they are the same but any is better for maney conditions
+
+6. If we need all the conditions to be true we will use ....
+
+all
+
+7. What the differences between if, elif
+
+elif for alternative condtions
+
+8.What is the differences between elif, else
+
+else when no condtion is ture
+
+9. can we use elif
+
+yes as much as we need
+
+10. Can we use more than one else
+
+no
+
+11. Write a simple ternary operator
+
+print ('welcome') if x == 1 else : print ('wrong')
+
+12. In elif, python will check all the conditions no matter what
+
+no python check one after one and takes the first right one
+
+13. In elif we use else for .....
+
+the condition when no condition is right
+
+14. if we have this list [2,4,6,8,10]:
+    1-check to see if 4 in this list or not
+    2- check to see if 4 and 6 in this list or not
+    3- check to see if 3 or 6 in this list or not
+    4- check to if 2,4 and 5 in this list or not
+
+
+    '''
