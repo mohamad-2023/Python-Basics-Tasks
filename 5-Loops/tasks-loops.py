88150eaf0409d74c407a79303ca026aed57b671d
mohamad-2023 <mmm19870linkin@gmail.com>
2022-11-12 14:50:39 +0100

    training for loops

--- 5-Loops/tasks-loops.py
+++ 5-Loops/tasks-loops.py
@@ -8,4 +8,40 @@ int((input("insert"))
 
  sometime need on loop
 
- '''
+x = int (input ('Enter the Number'))
+for x in range (1, x+1):
+    print (x)
+----------------
+
+ # a is the number of rows and b is the numbers in row
+a = int (input('Enter the number'))
+for a in range (1,a+1):
+    for b in range(1,a):
+        print (b,end ='')
+
+    print ('')
+
+---------------------------
+
+
+
+c = int(input('Enter the number'))
+d = sum (list(range(1,c+1)))
+
+print (d)
+# some thing went wrong and to solve it convert the range into a list and print the result
+
+
+
+
+f=0
+e = int (input ('Enter the number'))
+for e in range (1,e+1):
+    f+=e
+print('\n')
+print(f)
+
+'''
+
+
+
