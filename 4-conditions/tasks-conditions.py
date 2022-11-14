e330476319314f738b53f47ca26c71858032aa1b
Mohammad Mudallaleh <113829662+mohamad-2023@users.noreply.github.com>
2022-11-14 20:35:15 +0100

    Update tasks-conditions.py

--- 4-conditions/tasks-conditions.py
+++ 4-conditions/tasks-conditions.py
@@ -39,7 +39,7 @@ yes as much as we need
 
 10. Can we use more than one else
 
-no
+no but Nour  told me  that it works with 'if' as 'else if' and we will ask the teacher
 
 11. Write a simple ternary operator
 
