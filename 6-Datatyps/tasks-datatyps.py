2d683eecf47f48171a66c23915202727a8fb6d7e
mohamad-2023 <mmm19870linkin@gmail.com>
2022-11-17 07:31:13 +0100

    tasks-datatypes finsh

--- 6-Datatyps/tasks-datatyps.py
+++ 6-Datatyps/tasks-datatyps.py
@@ -28,7 +28,87 @@ print (y)
 
 6. Add 40 to the second element of the list
 
-'''
 y = list(range(10,21))
 y.append(30)
+y.insert(1,40)
 print (y)
+
+
+7. Print how many elements in the list
+
+
+y = list(range(10,21))
+y.append(30)
+y.insert(1,40)
+x = len(y)
+print (x)
+
+
+
+8. Replace the second element in the list with 100
+
+
+y = list(range(10,21))
+y.append(30)
+y.insert(1,40)
+y[1] = 100
+x = len(y)
+print (y)
+
+
+9. Create a tuple with values from 1 to 5
+
+t = tuple(range(1,6))
+print(t)
+
+
+
+10. Can we add 10 to the end of the tuple
+
+no we can not
+
+
+11. Create a dict of values Mahmoud:28,Ahmad:30
+
+d =  {'Mahmoud':28, 'Ahmad':30}
+print(d['Mahmoud'])
+
+
+12. Print Mahmoud age from dict
+
+13. What are the differences between mutable an immutable data types?
+
+mutable is changeable
+immutable means that can not be changed after we assind a value to it
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
