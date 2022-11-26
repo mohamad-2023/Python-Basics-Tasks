34340b0c109e7b7a1abdbf0a82ade19fa8ea6e4c
mohammad-2023 <mmm19780linkin@gmail.com>
2022-11-26 23:26:43 +0100

    add tasks-opp2 and remove tasks-oop1

---
+++ 8-oop/tasks-oop2.py
@@ -0,0 +1,24 @@
+'''
+1.Create a new class names SciCalc with 3 methonds, sum, mull, power all of them takes 2 arguments x,y
+
+2. Sum return the sum of x and y
+
+3. Mull return the multiplication of x and y
+
+4. The power return x power y
+
+5. Take an object from the class and call the 3 methods with any mumbers
+
+6. Can we inherit from the class we created in the first task Calc
+
+7. Injerit from the Calc class, mow remove the unneeded code lf the SciCslc after inheriting
+
+8. Call the 3 methods again from the SciCalc object
+
+9. Now you should see the same result as before
+
+10. Explain in few words what happened after inheriting
+
+
+
+''''
